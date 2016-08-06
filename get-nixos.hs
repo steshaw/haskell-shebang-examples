a156becf5f9f2933be5dd920818f530f951631cf
Steven Shaw <steven@steshaw.org>
2016-08-07 09:37:40 +1000

    get-nixos now works

--- get-nixos.hs
+++ get-nixos.hs
@@ -1,9 +1,9 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i runghc -p haskellPackages.ghc haskellPackages.HTTP
+#!/usr/bin/env nix-shell
+#!nix-shell -i runghc -p 'haskellPackages.ghcWithPackages (p: [p.HTTP])'
 
 import Network.HTTP
 
 main = do
-  resp <- Network.HTTP.simpleHTTP (getRequest "http://nixos.org/")
+  resp <- simpleHTTP (getRequest "http://nixos.org/")
   body <- getResponseBody resp
   print (take 100 body)
