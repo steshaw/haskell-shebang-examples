12c09d5983584a3fb5b9169564f46a0ebf17ce88
Steven Shaw <steven@steshaw.org>
2016-08-23 22:09:10 +1000

    Switch to `for_`

--- stack/resolveip.hs
+++ stack/resolveip.hs
@@ -1,7 +1,7 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-6.13 --install-ghc runghc
 
-import Control.Monad (forM_)
+import Data.Foldable (for_)
 import Network.BSD (getHostByName, hostAddresses)
 import Network.Socket (inet_ntoa)
 import System.Environment (getArgs)
@@ -12,6 +12,6 @@ resolve address = do
 
 main = do
   args <- getArgs
-  args `forM_` (\address -> do
+  for_ args $ \address -> do
     ips <- resolve address
-    ips `forM_` (\ip -> putStrLn $ address ++ "\t" ++ ip))
+    for_ ips $ \ip -> putStrLn $ address ++ "\t" ++ ip
