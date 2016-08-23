e41d7d59cee77655fadd808b17929ca0b5cc81c9
Steven Shaw <steven@steshaw.org>
2016-08-23 14:12:51 +1000

    MongoDB example

---
+++ stack/mongodb-teams.hs
@@ -0,0 +1,47 @@
+#!/usr/bin/env stack
+{- stack
+    --resolver lts-6.12
+    --install-ghc
+    runghc
+    --package mongoDB
+-}
+
+--
+-- Example code from
+-- https://hackage.haskell.org/package/mongoDB-2.1.1/docs/Database-MongoDB.html
+--
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+
+import Database.MongoDB
+import Control.Monad.Trans (liftIO)
+
+main = do
+   pipe <- connect (host "127.0.0.1")
+   e <- access pipe master "baseball" run
+   close pipe
+   print e
+
+run = do
+   clearTeams
+   insertTeams
+   allTeams >>= printDocs "All Teams"
+   nationalLeagueTeams >>= printDocs "National League Teams"
+   newYorkTeams >>= printDocs "New York Teams"
+
+clearTeams = delete (select [] "team")
+
+insertTeams = insertMany "team" [
+   ["name" =: "Yankees", "home" =: ["city" =: "New York", "state" =: "NY"], "league" =: "American"],
+   ["name" =: "Mets", "home" =: ["city" =: "New York", "state" =: "NY"], "league" =: "National"],
+   ["name" =: "Phillies", "home" =: ["city" =: "Philadelphia", "state" =: "PA"], "league" =: "National"],
+   ["name" =: "Red Sox", "home" =: ["city" =: "Boston", "state" =: "MA"], "league" =: "American"] ]
+
+allTeams = rest =<< find (select [] "team") {sort = ["home.city" =: 1]}
+
+nationalLeagueTeams = rest =<< find (select ["league" =: "National"] "team")
+
+newYorkTeams = rest =<< find (select ["home.state" =: "NY"] "team") {project = ["name" =: 1, "league" =: 1]}
+
+printDocs title docs = liftIO $ putStrLn title >> mapM_ (print . exclude ["_id"]) docs
